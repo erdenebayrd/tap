7022a570140df759b5f777ac57164b149a319941
erdenebayar <erdenebayr.d@gmail.com>
2024-10-17 23:28:09 +1100

    added firebase function & firestore

--- lib/login.dart
+++ lib/login.dart
@@ -1,6 +1,6 @@
 import "package:flutter/material.dart";
 import "package:flutter/services.dart";
-import "package:firebase_auth/firebase_auth.dart";
+// import "package:firebase_auth/firebase_auth.dart";
 // import "home.dart";
 
 class LoginPage extends StatefulWidget {
