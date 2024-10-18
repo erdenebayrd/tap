8a01a0e1a52ac596d38d63c32e9f3dc07eb2844d
erdenebayar <erdenebayr.d@gmail.com>
2024-10-18 14:23:40 +1100

    minor

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,9 @@ import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import "login.dart";
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  Firebase.initializeApp(
+  await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
 
