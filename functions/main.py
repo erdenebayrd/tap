aa4507b627b67c6eed39b4d67f2bbdb51272d1ff
erdenebayar <erdenebayr.d@gmail.com>
2024-10-18 14:12:29 +1100

    added function

--- functions/main.py
+++ functions/main.py
@@ -9,39 +9,23 @@ app = initialize_app()
 
 
 @https_fn.on_request()
-def addmessage(req: https_fn.Request) -> https_fn.Response:
-    """Take the text parameter passed to this HTTP endpoint and insert it into
-    a new document in the messages collection."""
-    # Grab the text parameter.
-    original = req.args.get("text")
-    if original is None:
-        return https_fn.Response("No text parameter provided", status=400)
-
-    firestore_client: google.cloud.firestore.Client = firestore.client()
-
-    # Push the new message into Cloud Firestore using the Firebase Admin SDK.
-    _, doc_ref = firestore_client.collection("messages").add({"original": original})
-
-    # Send back a message that we've successfully written the message
-    return https_fn.Response(f"Message with ID {doc_ref.id} added.")
+def alive(req: https_fn.Request) -> https_fn.Response:  # type: ignore
+    return https_fn.Response("I'm alive!")  # type: ignore
 
 
-@firestore_fn.on_document_created(document="messages/{pushId}")
-def makeuppercase(event: firestore_fn.Event[firestore_fn.DocumentSnapshot | None]) -> None:
-    """Listens for new documents to be added to /messages. If the document has
-    an "original" field, creates an "uppercase" field containg the contents of
-    "original" in upper case."""
+# @https_fn.on_request()
+# def addmessage(req: https_fn.Request) -> https_fn.Response:  # type: ignore
+#     """Take the text parameter passed to this HTTP endpoint and insert it into
+#     a new document in the messages collection."""
+#     # Grab the text parameter.
+#     original = req.args.get("text")
+#     if original is None:
+#         return https_fn.Response("No text parameter provided", status=400)  # type: ignore
 
-    # Get the value of "original" if it exists.
-    if event.data is None:
-        return
-    try:
-        original = event.data.get("original")
-    except KeyError:
-        # No "original" field, so do nothing.
-        return
+#     firestore_client: google.cloud.firestore.Client = firestore.client()
 
-    # Set the "uppercase" field.
-    print(f"Uppercasing {event.params['pushId']}: {original}")
-    upper = original.upper()
-    event.data.reference.update({"uppercase": upper})
+#     # Push the new message into Cloud Firestore using the Firebase Admin SDK.
+#     _, doc_ref = firestore_client.collection("messages").add({"original": original})
+
+#     # Send back a message that we've successfully written the message
+#     return https_fn.Response(f"Message with ID {doc_ref.id} added.")  # type: ignore
